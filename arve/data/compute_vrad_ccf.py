bug fix and implement fsr masking for ccf

arve/data/compute_vrad_ccf.py
@@ -13,6 +13,7 @@ class compute_vrad_ccf:
         criteria          : list[str]   | None = None ,
         exclude_tellurics : bool               = True ,
         exclude_regions   : bool               = True ,
+        limit_fsr         : bool               = False,
         ) -> None:
         """Compute radial velocities (RVs) from spectral data using the cross-correlation function (CCF) method.
 
@@ -30,6 +31,8 @@ class compute_vrad_ccf:
             exclude telluric bands, by default True
         exclude_regions : bool, optional
             exclude wavelength intervals, by default True
+        limit_fsr : bool, optional
+            limit calculation to the free spectral range of each order, by default False
 
         Returns
         -------
@@ -39,6 +42,7 @@ class compute_vrad_ccf:
 
         # read data
         wave_val = self.spec["wave_val"]
+        N_spec   = self.spec["N_spec"]
         N_ord    = self.spec["N_ord"]
 
         # read constants
@@ -79,6 +83,13 @@ class compute_vrad_ccf:
             else:
                 criteria.append("crit_excl")
 
+        # limit to free spectral range
+        if limit_fsr:
+            if criteria is None:
+                criteria = ["crit_fsr"]
+            else:
+                criteria.append("crit_fsr")
+
         # lines which satisfy criteria
         if criteria is not None:
             for i in range(N_ord):
@@ -101,6 +112,7 @@ class compute_vrad_ccf:
         else:
             ccf_vrad       = np.arange(vrad_grid[0], vrad_grid[1]+vrad_grid[2]/2, vrad_grid[2])
             ccf_err_corr   = (vrad_step_med/vrad_grid[2])**(1/2)
+        N_vrad = len(ccf_vrad)
 
         # keep mask lines within spectrum overlap
         for i in range(N_ord):
@@ -112,13 +124,6 @@ class compute_vrad_ccf:
         # normalize weights
         for i in range(N_ord):
             weight[i] = weight[i]/np.sum(weight[i])
-        
-        # nr. of spectra, RV shifts and lines
-        if self.spec["path"] is None:
-            N_spec = len(self.spec["flux_val"])
-        else:
-            N_spec = len(self.spec["files"])
-        N_vrad = len(ccf_vrad)
 
         # empty arrays for indices
         i_l = np.zeros((N_ord,N_vrad), dtype=object)